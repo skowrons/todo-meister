2397d7a5b6aca117c86ad8d4cdabb161b8eb8472
Maximilian Skowron <derskoffi@gmail.com>
2023-04-16 19:07:42 +0200

    feat(config): allow to load from config file

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,23 +1,54 @@
 package config
 
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
 type Config struct {
-	Ignores map[string]any
-	Fields  []Field
+	Ignores map[string]any `yaml:"ignores"`
+	Fields  []Field        `yaml:"fields"`
+
+	// ManageLabel will the label used by todo meister to track if this issue
+	// is managed by todo meister. Defaults to managed-by::tm
+	ManageLabel string `yaml:"manageLabel"`
 }
 
 type Field struct {
 	// Text is the text that will be matched bei todo-meister
-	Text string
+	Text string `yaml:"text"`
 
 	// Labels represent the labels concept used by many issue tools.
 	// In the end the connectors will decide what to do with the labels.
 	// e.x. Jira -> priority:minor could be transalted to the native priority field of a jira issue
-	Labels []string
+	Labels []string `yaml:"labels"`
 }
 
-// TODO: load from config file
 func LoadConfig() *Config {
-	return StdConfig()
+	cfg, err := readConfigFile()
+	if err != nil {
+		return StdConfig()
+	}
+
+	return cfg
+}
+
+// readConfigFile attempts to read the config file from disk.
+func readConfigFile() (*Config, error) {
+	for _, name := range []string{"tm.yaml", "tm.config.yaml"} {
+		if data, err := os.ReadFile(name); err == nil {
+			var cfg *Config
+			if err := yaml.Unmarshal(data, &cfg); err != nil {
+				// TODO: maybe provide a better message to the cli user
+				return nil, os.ErrNotExist
+			}
+			return cfg, nil
+		}
+	}
+
+	// Config file not found
+	return nil, os.ErrNotExist
 }
 
 func StdConfig() *Config {
@@ -28,6 +59,7 @@ func StdConfig() *Config {
 			"venv":         "",
 			".git":         "",
 		},
+		ManageLabel: "managed-by::tm",
 		Fields: []Field{
 			{
 				Text: "TODO:",
